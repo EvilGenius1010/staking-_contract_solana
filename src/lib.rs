c95dc6b50867ea72b45bda50a25d91433527922a
EvilGenius1010 <kolhatkarh@gmail.com>
2025-08-27 03:33:12 +0530

    added instruction fns, parsing types

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,53 @@
+use borsh::{BorshDeserialize, BorshSerialize};
 use solana_program::{
-    account_info::{next_account_info, AccountInfo}, entrypoint::{self, ProgramResult}, msg, pubkey::Pubkey,
-    stake::instruction::create_account,
-    program_error::ProgramError::MissingRequiredSignature,
-    progra
+    account_info::{next_account_info, AccountInfo}, entrypoint::{self, ProgramResult}, msg, program_error::ProgramError::{self, InvalidInstructionData, MissingRequiredSignature}, pubkey::Pubkey, stake::instruction::create_account
+    // progra
 };
 
-entrypoint!(process_instruction);
+#[derive(BorshSerialize,BorshDeserialize)]
+struct SetupAccountStruct{
+    payer:Pubkey,
+    amount:u64
+}
 
-pub fn process_instruction(
+
+#[derive(BorshSerialize,BorshDeserialize)]
+struct DelegateAccountStruct{
+    validator:Pubkey,
+    payer:Pubkey
+}
+
+// #[derive(BorshSerialize,BorshDeserialize)]
+// struct RevokeAuthority{
+
+// }
+
+// // #[derive(BorshSerialize,BorshDeserialize)]
+// // struct UpdateAmounts{
+
+// // }
+
+// // #[derive(BorshSerialize,BorshDeserialize)]
+// // struct SetupAndDelegate{
+
+// // }
+
+
+#[derive(BorshSerialize,BorshDeserialize)]
+enum Instructions{
+  Setup(SetupAccountStruct),
+  Delegate(DelegateAccountStruct),
+//   Update(UpdateAmounts),
+//   SetupAndDelegate(SetupAndDelegate)
+}
+
+
+entrypoint!(entry_instruction);
+
+pub fn entry_instruction(
     program_id: &Pubkey,
     accounts: &[AccountInfo],
-    _ix_data: &[u8],
+    ix_data: &[u8],
 ) -> ProgramResult {
 
     // create_account(from_pubkey, stake_pubkey, authorized, lockup, lamports)
@@ -20,12 +57,38 @@ pub fn process_instruction(
     let stake_account = next_account_info(accounts_iterator)?;
     let system_program = next_account_info(accounts_iterator)?;
 
+    let instruction_type = Instructions::try_from_slice(ix_data);
+
+
+    match instruction_type{
+        Ok(Instructions::Setup(SetupAccountStruct))=>{
+            SetupAccount(SetupAccountStruct,payer);
+        },
+        Ok(Instructions::Delegate(DelegateAccountStruct))=>{
+            Delegate(DelegateAccountStruct);
+        },
+        Err(errormsg)=>{
+            msg!("Invalid Instruction! Error is { }",errormsg);
+            return Err(InvalidInstructionData);
+        }
+    }   
+
     if !payer.is_signer { return Err(MissingRequiredSignature); }
 
-    if stake_account.owner != program_id { msg!("Not our account"); return Err(ProgramError::IncorrectProgramId); }
+    // if stake_account.owner != program_id { msg!("Not our account"); return Err(ProgramError::IncorrectProgramId); }
     let mut data = stake_account.data.borrow_mut();    
 
 
     msg!("Hello, world!");
     Ok(())
+}
+
+
+fn SetupAccount(ix_data:SetupAccountStruct,payer:&AccountData)->Result<Instructions, ProgramError>{
+    assert!(payer.is_signer);
+
+}
+
+fn Delegate(ixdata:DelegateAccountStruct)->Result<Instructions,ProgramError>{
+
 }
